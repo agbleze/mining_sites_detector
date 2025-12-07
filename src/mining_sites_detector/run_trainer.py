72b74387db28100452150e4c389ce85d612d1d48
linus <lagbleze@st.ug.edu.gh>
2025-12-07 22:36:24 +0100

    feat: add export_to_onnx

--- src/mining_sites_detector/run_trainer.py
+++ src/mining_sites_detector/run_trainer.py
@@ -2,7 +2,9 @@ from argparse import ArgumentParser
 import os
 from mining_sites_detector.model_trainer import trigger_training_process, get_model
 from mining_sites_detector.data_preprocessor import get_data, get_tiff_img
+from mining_sites_detector.export_utils import export_onnx_model
 import json
+import torch
 
 
 def parse_args():
@@ -17,6 +19,7 @@ def parse_args():
     parser.add_argument('--model_name', type=str, default="mining_site_detector_model", help='Base name for the saved model files')
     parser.add_argument('--normalize_bands', action='store_true', help='Whether to normalize image bands')
     parser.add_argument('--checkpoint_interval', type=int, default=1, help='Interval (in epochs) to save model checkpoints')
+    parser.add_argument('--export_to_onnx', action='store_true', help='Whether to export the trained model to ONNX format after training')
     return parser.parse_args()
 
 
@@ -31,6 +34,7 @@ def main():
     model_store_dir = args.model_store_dir
     model_name = args.model_name
     checkpoint_interval = args.checkpoint_interval
+    export_to_onnx = args.export_to_onnx
     
     train_dl, val_dl = get_data(train_img_dir=train_img_dir, 
                             val_image_dir=val_img_dir,
@@ -55,6 +59,17 @@ def main():
     
     with open(save_train_results_as, "w") as f:
         json.dump(result, f)
+        
+    if export_to_onnx:
+        final_model_path = os.path.join(model_store_dir, f'{model_name}_epoch_{num_epochs}.pth')
+        model.load_state_dict(torch.load(final_model_path))
+        sample_img, _ = next(iter(train_dl))
+        sample_image = sample_img[0]
+        onnx_save_path = os.path.join(model_store_dir, f"{model_name}_epoch_{num_epochs}.onnx")
+        export_onnx_model(model=model, image=sample_image, save_onnx_as=onnx_save_path)
+        print(f"Exported ONNX model to: {onnx_save_path}")
+        
+    
 
 if __name__ == "__main__":
     main()   
