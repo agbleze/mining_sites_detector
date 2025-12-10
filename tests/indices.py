547296df521a678b861231c97ec49b47bc686e51
linus <lagbleze@st.ug.edu.gh>
2025-12-10 16:39:04 +0100

    feat: update indices

--- tests/indices.py
+++ tests/indices.py
@@ -148,7 +148,86 @@ class Sentinel2Dataset(object):
         self.gossan = band11 / band4
         return self.gossan
     
-
+    def compute_modified_baresoil_index(self, img):
+        """
+        Compute the Modified Baresoil Index (MBI) for the given image.
+        Modified Bare Soil Index (MBI) for Sentinel-2 can be 
+        computed using the SWIR1, SWIR2, and NIR spectral bands to improve 
+        the discrimination between bare soil and built-up areas.
+        
+        Help reduce the misclassification of built-up areas as bare soil, a common issue with the standard BSI. 
+        The resulting values for bare soil are typically positive and higher than for other land
+        
+        """
+        B12_index = self.all_band_names.index("B12")
+        B8_index = self.all_band_names.index("B08")
+        B11_index = self.all_band_names.index("B11")
+        band12 = img[:, :, B12_index]
+        band8 = img[:, :, B8_index]
+        band11 = img[:, :, B11_index]   
+        mbi_numerator = band11 - band12 - band8
+        mbi_denominator = band11 + band12 + band8     
+        self.modified_baresoil = (mbi_numerator / mbi_denominator) + 0.5
+        return self.modified_baresoil
+    
+    def compute_3BUI_index(self, img):
+        """
+        Compute the 3-Band Built-Up Index (3BUI) for the given image.
+        The 3-Band Built-Up Index (3BUI) is designed to enhance the detection 
+        of built-up areas using three spectral bands: Red, NIR, and SWIR1.
+        
+        Equation is cited from: 
+        """
+        B4_index = self.all_band_names.index("B04")
+        B8_index = self.all_band_names.index("B08")
+        B11_index = self.all_band_names.index("B11")
+        band4 = img[:, :, B4_index]
+        band8 = img[:, :, B8_index]
+        band11 = img[:, :, B11_index]   
+        self.three_band_urband_index =  band4 + band11 - band8  
+        return self.three_band_urband_index
+    
+    
+    def compute_BAEI(self, img):
+        """
+        Compute the Built-Up Area Extraction Index (BAEI) for the given image.
+        The Built-Up Area Extraction Index (BAEI) is designed to enhance the detection 
+        of built-up areas using three spectral bands: Red, NIR, and SWIR1.
+        
+        Equation is cited from: 
+        """
+        B4_index = self.all_band_names.index("B04")
+        B3_index = self.all_band_names.index("B03")
+        B11_index = self.all_band_names.index("B11")
+        band4 = img[:, :, B4_index]
+        band3 = img[:, :, B3_index]
+        band11 = img[:, :, B11_index]   
+        baei_numerator = band4 + 0.3
+        baei_denominator = band3 + band11
+        self.built_up_area_extraction_index = baei_numerator / baei_denominator
+        return self.built_up_area_extraction_index
+    
+    def compute_BBI(self, img):
+        """
+        Built-up and Bare Land Index
+        
+        Equation is cited from: 
+        """
+        B2_index = self.all_band_names.index("B02")
+        B3_index = self.all_band_names.index("B03")
+        B4_index = self.all_band_names.index("B04")
+        
+        band4 = img[:, :, B4_index]
+        band2 = img[:, :, B2_index]
+        band3 = img[:, :, B3_index]   
+        leftside_bbi_numerator = band2 - band3
+        leftside_bbi_denominator = band2 + band3
+        rightside_bbi_numerator = band4 - band3
+        rightside_bbi_denominator = band4 + band3
+        leftside_bbi = leftside_bbi_numerator / leftside_bbi_denominator
+        rightside_bbi = rightside_bbi_numerator / rightside_bbi_denominator
+        self.built_up_bare_land_index = leftside_bbi + rightside_bbi
+        return self.built_up_bare_land_index
 
 #%%
 
@@ -203,3 +282,42 @@ image = torch.clamp(indices_stack / 10000, min=0, max=1).numpy()
 fig, ax = plt.subplots()
 ax.imshow(indices_stack)
 # %%
+sample_img.dtype
+# %%
+indices_stack.max()
+# %%
+gossan_index.max()
+# %%
+ferric_oxide.max()
+# %%
+iron_oxide = dataset.cal_iron_oxide_index(img=sample_img)
+# %%
+iron_oxide.max()
+# %%
+from sklearn.preprocessing import StandardScaler
+# %%
+scaler = StandardScaler()
+# %%
+scaled_indices = scaler.fit_transform(indices_stack.reshape(-1, indices_stack.shape[-1]))
+# %%
+scaled_indices.min()
+# %%
+#import torch.functional as F
+import torch.nn.functional as F
+# %%
+F.normalize(torch.tensor(indices_stack), dim=0).numpy().min()
+# %%
+from torchvision import transforms
+# %%
+transforms.Normalize()
+
+# %%
+import numpy as np
+
+def compute_mean_std_numpy(arrs):
+    # arrs: list of arrays shaped (bands, H, W)
+    stacked = np.concatenate([a.reshape(a.shape[0], -1) for a in arrs], axis=1)
+    mean = stacked.mean(axis=1)
+    std  = stacked.std(axis=1)
+    return mean, std
+
