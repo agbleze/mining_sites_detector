c3b1e26ee6634b65004dcb5fccd86300c5255022
linus <lagbleze@st.ug.edu.gh>
2026-01-15 23:54:41 +0100

    fix: update inceptionV2Block

--- src/mining_sites_detector/inception_v2.py
+++ src/mining_sites_detector/inception_v2.py
@@ -12,6 +12,8 @@ class inceptionV2Block(nn.Module):
         super().__init__()
         
         self.zeropad = nn.ZeroPad2d()
+        self.maxpool = nn.MaxPool2d(kernel_size=3)
+        self.conv1x1_a = nn.Conv2d(kernel_size=1, stride=1, padding="same")
         
 
 
