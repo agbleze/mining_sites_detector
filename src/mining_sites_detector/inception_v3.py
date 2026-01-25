19f6bc1583303dac2d50452fe437fc4892f8a0c8
linus <lagbleze@st.ug.edu.gh>
2026-01-25 01:12:59 +0100

    fix: add forward method

--- src/mining_sites_detector/inception_v3.py
+++ src/mining_sites_detector/inception_v3.py
@@ -51,8 +51,6 @@ class InceptionV3Stem(nn.Module):
         return x
         
     
-        
-
 
 
 class InceptionV3BlockA(nn.Module):
@@ -61,10 +59,55 @@ class InceptionV3BlockA(nn.Module):
         
         self.act = nn.ReLU()
         self.zeropad1 = nn.ZeroPad2d(1)
-        self.zeropad2 = nn.ZeroPad2d(2)
+        #self.zeropad2 = nn.ZeroPad2d(2)
         self.bn = nn.BatchNorm2d()
+        self.avgpool = nn.AvgPool2d(kernel_size=3, stride=1, padding="same")
         
         self.f1x1_conv = nn.LazyConv2d(out_channels=f1x1[0], kernel_size=1, stride=1, padding="same", bias=False)
+        
         self.f3x3_conv1x1 = nn.LazyConv2d(out_channels=f3x3[0], kernel_size=1, padding="same", stride=1, bias=False)
+        self.f3x3_conv3x3 = nn.LazyConv2d(out_channels=f3x3[1], kernel_size=3, stride=1, padding="same", bias=False)
+        
+        self.f5x5_conv1x1 = nn.LazyConv2d(out_channels=f5x5[0], kernel_size=1, stride=1, padding="same", bias=False)
+        self.f5x5_conv3x3_a = nn.LazyConv2d(out_channels=f5x5[1], kernel_size=3, stride=1, padding="same", bias=False)
+        self.f5x5_conv3x3_b = nn.LazyConv2d(out_channels=f5x5[2], kernel_size=3, stride=1, padding="same", bias=False)
+        
+        self.pool_conv1x1 = nn.LazyConv2d(out_channels=fpool[0], kernel_size=1, stride=1, padding="same", bias=False)
+        
+        
+    def forward(self, x):
+        x_f1x1 = self.f1x1_conv(x)
+        x_f1x1 = self.bn(x_f1x1)
+        x_f1x1 = self.act(x_f1x1)
+
+        x_f3x3 = self.f3x3_conv1x1(x)
+        x_f3x3 = self.bn(x_f3x3)
+        x_f3x3 = self.act(x_f3x3)
+        #x_f3x3 = self.zeropad1(x_f3x3)
+        x_f3x3 = self.f3x3_conv3x3(x_f3x3)
+        
+        x_f5x5 = self.f5x5_conv1x1(x)
+        x_f5x5 = self.bn(x_f5x5)
+        x_f5x5 = self.act(x_f5x5)
+        
+        x_f5x5 = self.f5x5_conv3x3_a(x_f5x5)
+        x_f5x5 = self.bn(x_f5x5)
+        x_f5x5 = self.act(x_f5x5)
+        
+        x_f5x5 = self.f5x5_conv3x3_b(x_f5x5)
+        x_f5x5 = self.bn(x_f5x5)
+        x_f5x5 = self.act(x_f5x5)
+        
+        
+        x_pool = self.avgpool(x)
+        x_pool = self.pool_conv1x1(x_pool)
+        x_pool = self.bn(x_pool)
+        x_pool = self.act(x_pool)
+        
+        output = torch.concat([x_f1x1, x_f3x3, x_f5x5, x_pool], dim=1)
+        return output
+        
+        
+        
         
     
