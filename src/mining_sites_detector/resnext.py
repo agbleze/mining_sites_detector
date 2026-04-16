8cb5d733317379e9572d679fdd94875cecca7af0
linus <lagbleze@st.ug.edu.gh>
2026-04-16 23:12:14 +0200

    fix: update to ResNextStem

--- src/mining_sites_detector/resnext.py
+++ src/mining_sites_detector/resnext.py
@@ -2,14 +2,18 @@ import torch.nn as nn
 import torch
 
 
-class RestNextStem(nn.Module):
+class ResNextStem(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv = nn.LazyConv2d(out_channels=64, kernel_size=7, stride=2)
+        self.conv = nn.LazyConv2d(out_channels=64, kernel_size=7, stride=2, bias=False)
         self.maxpool = nn.MaxPool2d(kernel_size=2)
+        self.bn = nn.BatchNorm2d()
+        self.act = nn.ReLU()
         
     def forward(self, x):
         x = self.conv(x)
+        x = self.bn(x)
+        x = self.act(x)
         x = self.maxpool(x)
         return x
         
