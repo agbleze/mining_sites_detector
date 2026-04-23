93e64102d58da7955a0e689335acc5d2eba2509a
linus <lagbleze@st.ug.edu.gh>
2026-04-23 23:27:49 +0200

    feat: add WRNProjectionBlock

--- src/mining_sites_detector/wrn.py
+++ src/mining_sites_detector/wrn.py
@@ -39,4 +39,87 @@ class WRNClassifier(nn.Module):
         
         
             
-
+class WRNIdentityBlock(nn.Module):
+    def __init__(self, n_filters, k, dropout_rate, l=1):
+        """n_filters: number of filters in the convolutional layers
+           k: widening factor
+           l: number of convolutional layers in the block
+           dropout_rate: dropout rate for regularization
+        """
+        super().__init__()
+        
+        if l < 2:
+            raise ValueError("Number of convolutional layers in the block must be at least 2.")
+        
+        conv = nn.Sequential(nn.LazyBatchNorm2d(),
+                                nn.ReLU(),
+                                nn.LazyConv2d(out_channels=n_filters*k, kernel_size=3),
+                                
+                                #nn.Dropout(dropout_rate),
+                                
+                                
+                            )
+        
+        block_conv = []
+        
+        for _ in range(l):
+            block_conv.append(conv)
+            block_conv.append(nn.Dropout(dropout_rate))
+        
+        block_conv = block_conv[:-1]  # Remove the last dropout layer
+        self.block_conv = nn.Sequential(*block_conv)
+        
+        
+    def forward(self, x):
+        shortcut = x
+        x = self.block_conv(x)
+        x += shortcut
+        return x
+        
+        
+        
+class WRNProjectionBlock(nn.Module):
+    def __init__(self, n_filters, k, dropout_rate, l=2):
+        """
+        n_filters: number of filters in the convolutional layers
+        k: widening factor
+        l: number of convolutional layers in the block
+        dropout_rate: dropout rate for regularization
+        """
+        
+        super().__init__()
+        
+        if l < 2:
+            raise ValueError("Number of convolutional layers in the block must be at least 2.")
+        
+        self.proj = nn.Sequential(
+                                    nn.LazyBatchNorm2d(),
+                                    nn.ReLU(),
+                                    nn.LazyConv2d(out_channels=n_filters*k, kernel_size=1)
+                                )        
+        
+        block_convs = []
+        
+        conv = nn.Sequential(nn.LazyBatchNorm2d(),
+                             nn.ReLU(),
+                             nn.LazyConv2d(out_channels=n_filters*k, kernel_size=3)
+                             )
+        for _ in range(l):
+            block_convs.append(conv)
+            block_convs.append(nn.Dropout(dropout_rate))
+            
+        block_convs = block_convs[:-1]  # Remove the last dropout layer
+        self.block_conv = nn.Sequential(*block_convs)
+        
+        
+    def forward(self, x):
+        shortcut = self.proj(x)
+        
+        x = self.block_conv(x)
+        x += shortcut
+        return x
+        
+        
+        
+        
+    
