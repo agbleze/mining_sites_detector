5d6bf3a1cdec0e46798f26547017f726917ba05f
linus <lagbleze@st.ug.edu.gh>
2026-04-30 00:24:10 +0200

    feat: update XceptionStem

--- src/mining_sites_detector/xception.py
+++ src/mining_sites_detector/xception.py
@@ -5,4 +5,13 @@ from torchsummary import summary
 
 
 class XceptionStem(nn.Module):
-    pass
+    def __init__(self, n_filters):
+        super().__init__()
+        
+        self.conv1 = nn.Sequential(
+                                nn.LazyConv2d(n_filters, kernel_size=3, stride=2, padding=1, bias=False),
+                                nn.LazyBatchNorm2d(),
+                                nn.ReLU(inplace=True)
+                            )
+        
+        
