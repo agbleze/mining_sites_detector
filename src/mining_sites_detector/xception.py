17463426e1347be037e934b3cde416ed58694c07
linus <lagbleze@st.ug.edu.gh>
2026-05-02 01:47:37 +0200

    feat: add make_model

--- src/mining_sites_detector/xception.py
+++ src/mining_sites_detector/xception.py
@@ -218,8 +218,42 @@ class Classifier(nn.Module):
         return x
     
     
+def kernel_initializer(m, kernel_initializer="he_normal"):
+    """Kernel initializer to be used on initializing Lazy modules, using He normal initialization for convolutional layers and Xavier uniform initialization for linear layers
+    Args:  m (nn.Module): module to initialize
+        kernel_initializer (str, optional): type of kernel initializer to use. Defaults to 'he_normal'. Options are 'he_normal' and 'glorot_uniform'
+    Returns:    
+        None
+    """
+    if isinstance(m, nn.LazyConv2d) or isinstance(m, nn.LazyLinear) or isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+        if kernel_initializer == "he_normal":
+            nn.init.kaiming_normal_(m.weight, mode="fan_in", nonlinearity="relu")
+        elif kernel_initializer == "glorot_uniform":
+            nn.init.xavier_uniform_(m.weight)
+        if m.bias is not None:
+            nn.init.zeros_(m.bias)
+                
+                
+def make_model(num_classes, data, device="cuda"):
+    entryflow = EntryFlow()
+    middleflow = MiddleFlowBlock()
+    exitflow = ExitFlow()
+    classifier = Classifier(num_classes=10)
+    model = nn.Sequential(entryflow, middleflow, exitflow, classifier)   
+    model.to(device)
+    _ = model(data.to(device))
+    model.apply(kernel_initializer)
+    return model
+    
+                 
+                
+
+
+
     
     
-    
-    
+data = torch.randn(1, 3, 299, 299)
+x = entry(data)
+x = middle(x)
+x = exit(x)    
         
