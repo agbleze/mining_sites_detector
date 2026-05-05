1c7699e6f6fb771b77bfea7a90e30db791e94e0f
linus <lagbleze@st.ug.edu.gh>
2026-05-05 22:53:42 +0200

    feat: add SenetStem

--- src/mining_sites_detector/senet.py
+++ src/mining_sites_detector/senet.py
@@ -1,2 +1,20 @@
 import torch
 import torch.nn as nn
+
+
+
+class SenetStem(nn.Module):
+    def __init__(self, ):
+        super().__init__()
+        
+        self.conv1 = nn.LazyConv2d(out_channels=64, 
+                                   kernel_size=7,
+                                   stride=2
+                                   )
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2,
+                                    )
+        
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.maxpool(x)
+        return x
