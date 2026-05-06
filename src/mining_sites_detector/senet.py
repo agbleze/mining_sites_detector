6972ac890b74f818f288e22a230106ab149038e5
linus <lagbleze@st.ug.edu.gh>
2026-05-06 23:53:51 +0200

    feat: add SequeezeExcitationBlock

--- src/mining_sites_detector/senet.py
+++ src/mining_sites_detector/senet.py
@@ -18,3 +18,28 @@ class SenetStem(nn.Module):
         x = self.conv1(x)
         x = self.maxpool(x)
         return x
+    
+    
+class SequeezeExcitationBlock(nn.Module):
+    def __init__(self, r):
+        super().__init__()
+        self.global_avgpool = nn.AdaptiveAvgPool2d((1,1))
+        # 1x1 x C/r
+        # need to infer the number of channels in the input feature map 
+        # to determine the output features for this layer
+        self.fc1 = nn.LazyLinear(out_features=r)
+        self.relu = nn.ReLU()
+        
+        self.fc2 = nn.LazyLinear()
+        self.sigmoid = nn.Sigmoid()
+        
+    def forward(self, x):
+        shortcut = x
+        x = self.global_avgpool(x)
+        x = torch.flatten(x, start_dim=1)
+        x = self.fc1(x)
+        self.relu(x)
+        self.fc2(x)
+        x = self.sigmoid(x)
+        x = shortcut * x 
+        return x     
