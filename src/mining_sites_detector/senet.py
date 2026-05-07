a45414cb14e2838899d61007cddc27734cfd6aa3
linus <lagbleze@st.ug.edu.gh>
2026-05-08 00:42:21 +0200

    feat: add ExcitationBlock

--- src/mining_sites_detector/senet.py
+++ src/mining_sites_detector/senet.py
@@ -2,44 +2,74 @@ import torch
 import torch.nn as nn
 
 
-
 class SenetStem(nn.Module):
     def __init__(self, ):
         super().__init__()
         
         self.conv1 = nn.LazyConv2d(out_channels=64, 
                                    kernel_size=7,
-                                   stride=2
+                                   stride=2,
+                                   padding=3
                                    )
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2,
+                                    padding=1
                                     )
         
     def forward(self, x):
         x = self.conv1(x)
         x = self.maxpool(x)
         return x
-    
-    
-class SequeezeExcitationBlock(nn.Module):
+
+class SqueezeBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.global_avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        
+    def forward(self, x):
+        x = self.global_avgpool(x)
+        x = torch.flatten(x, start_dim=1)
+        return x
+        
+
+
+
+class ExcitationBlock(nn.Module):
     def __init__(self, reduction_ratio):
         super().__init__()
-        self.global_avgpool = nn.AdaptiveAvgPool2d((1,1))
-        # 1x1 x C/r
-        # need to infer the number of channels in the input feature map 
-        # to determine the output features for this layer
-        self.fc1 = nn.LazyLinear(out_features=reduction_ratio)
+        self.reduction_ratio = reduction_ratio
+        self.fc1 = nn.LazyLinear(out_features=None)
         self.relu = nn.ReLU()
         
-        self.fc2 = nn.LazyLinear()
+        self.fc2 = nn.LazyLinear(out_features=None)
         self.sigmoid = nn.Sigmoid()
+        
+    def initialize_parameters(self, input):
+        C = input.shape[1]
+        reduced_channels = C // self.reduction_ratio
+        
+        self.fc1.out_features = reduced_channels
+        self.fc2.out_features = C
+        
+        super().initialize_parameters(input)
+        
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.sigmoid(x)
+        return x 
+    
+class SqueezeExcitationBlock(nn.Module):
+    def __init__(self, reduction_ratio):
+        super().__init__()
+        self.squeeze = SqueezeBlock()
+        self.excitation = ExcitationBlock(reduction_ratio=reduction_ratio)
+        
         
     def forward(self, x):
         shortcut = x
-        x = self.global_avgpool(x)
-        x = torch.flatten(x, start_dim=1)
-        x = self.fc1(x)
-        self.relu(x)
-        self.fc2(x)
-        x = self.sigmoid(x)
-        x = shortcut * x 
-        return x     
+        x = self.squeeze(x)
+        x = self.excitation(x)
+        x = x.unsqueeze(-1).unsqueeze(-1)
+        x = shortcut * x
+        return x
