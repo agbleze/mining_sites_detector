c9f094a4d0ed001c74dea35b6b74b575292a8b11
linus <lagbleze@st.ug.edu.gh>
2026-05-09 22:26:31 +0200

    fix: add stride param to group and learner

--- src/mining_sites_detector/senet.py
+++ src/mining_sites_detector/senet.py
@@ -44,7 +44,6 @@ class SELazyLinear(nn.LazyLinear):
                  ):
         super().__init__(out_features=None, bias=bias)
         self.reduction_ratio = reduction_ratio
-        #print(f"reduction_ratio: {self.reduction_ratio}")
         self.mode = mode
         self.target_out = target_out  # used for fc2 to know final C
         #print(f"out_features: {self.out_features}")
@@ -56,17 +55,10 @@ class SELazyLinear(nn.LazyLinear):
             out_ch = max(1, in_ch // self.reduction_ratio)
             self.in_features = in_ch
             self.out_features = out_ch
-            print(f"IN MODE: {self.mode}")
-            print(f"reduction_ratio : {self.reduction_ratio}")
-            print(f"in_features: {self.in_features}")
-            print(f"out_features: {self.out_features}")
-        elif self.mode == "expand":  # expand
-            # if target_out provided, use it; otherwise expand to in_ch (fallback)
-            print(f"IN MODE: {self.mode}")
+        elif self.mode == "expand":
             self.in_features = in_ch
             self.out_features = self.target_out if self.target_out is not None else in_ch
             self.out_features = int(self.in_features * self.reduction_ratio)
-            print(f"else in_features: {self.in_features}  out_features: {self.out_features}")
         super().initialize_parameters(input)
 
         
@@ -76,48 +68,45 @@ class SqueezeBlock(nn.Module):
         self.global_avgpool = nn.AdaptiveAvgPool2d((1, 1))
         
     def forward(self, x):
-        print(f"Squeeze block input shape before global_avgpool: {x.shape}")
         x = self.global_avgpool(x)
-        print(f"Squeeze block output shape after global_avgpool: {x.shape}")
         x = torch.flatten(x, start_dim=1)
-        print(f"Squeeze block output shape after flatten: {x.shape}")
         return x
 
 class ExcitationBlock(nn.Module):
     def __init__(self, reduction_ratio):
         super().__init__()
         self.reduction_ratio = reduction_ratio
-        self.fc1 = SELazyLinear(reduction_ratio=reduction_ratio, mode="reduce")
+        self.fc1 = SELazyLinear(reduction_ratio=reduction_ratio, mode="reduce", 
+                                bias=False
+                                )
         self.relu = nn.ReLU()
         
-        self.fc2 = SELazyLinear(target_out=None, mode="expand", reduction_ratio=reduction_ratio)
+        self.fc2 = SELazyLinear(target_out=None, mode="expand", reduction_ratio=reduction_ratio, 
+                                bias=False
+                                )
         self.sigmoid = nn.Sigmoid()
         
         if getattr(self.fc1, "in_features", None) is not None and self.fc2.target_out is None:
             self.fc2.target_out = self.fc1.in_features
         
-    def initialize_parameters(self, input):
-        print(f"Initializing Excitation block parameters with input shape: {input.shape}")
-        x = input[0]
-        C = int(x.shape[1])
-        print(f"Excitation block input channels: {C}")
-        reduced_channels = C // self.reduction_ratio
-        print(f"Excitation block reduced channels: {reduced_channels}")
+    # def initialize_parameters(self, input):
+    #     print(f"Initializing Excitation block parameters with input shape: {input.shape}")
+    #     x = input[0]
+    #     C = int(x.shape[1])
+    #     print(f"Excitation block input channels: {C}")
+    #     reduced_channels = C // self.reduction_ratio
+    #     print(f"Excitation block reduced channels: {reduced_channels}")
         
-        self.fc1.out_features = reduced_channels
-        self.fc2.out_features = C
+    #     self.fc1.out_features = reduced_channels
+    #     self.fc2.out_features = C
         
-        super().initialize_parameters(input)
+    #     super().initialize_parameters(input)
         
     def forward(self, x):
-        print(f"Excitation block input shape before fc1: {x.shape}")
         x = self.fc1(x)
-        print(f"Excitation block after fc1: {x.shape}")
         x = self.relu(x)
         x = self.fc2(x)
-        print(f"Excitation block after fc2: {x.shape}")
         x = self.sigmoid(x)
-        print(f"Excitation block after sigmoid: {x.shape}")
         return x 
     
 class SqueezeExcitationBlock(nn.Module):
@@ -126,17 +115,11 @@ class SqueezeExcitationBlock(nn.Module):
         self.squeeze = SqueezeBlock()
         self.excitation = ExcitationBlock(reduction_ratio=reduction_ratio)
         
-        
     def forward(self, x):
         shortcut = x
-        print(f"Input shape to SE block: {x.shape}")
         x = self.squeeze(x)
-        print(f"Squeeze block output shape: {x.shape}")
         x = self.excitation(x)
-        print(f"x after excitation: {x.shape}")
         x = x.unsqueeze(-1).unsqueeze(-1)
-        #x = x.view(x.size(0), x.size(1), 1, 1)  # reshape to (B, C, 1, 1)
-        print(f"x = x.unsqueeze(-1).unsqueeze(-1) Excitation output after unsqueeze shape: {x.shape}")
         x = shortcut * x
         return x
     
@@ -144,7 +127,7 @@ class SqueezeExcitationBlock(nn.Module):
 class SEResNextProjectionBlock(nn.Module):
     def __init__(self, bottleneck_width, out_channels, cardinality=32, reduction_ratio=16,
                  stride=1,
-                 variant: Literal["standard", "pre", "post", "identity"] = "standard"
+                 variant: Literal["standard", "pre", "post", "identity", "enclose"] = "standard"
                  ):
         super().__init__()
         if variant not in VARIANT_OPTIONS:
@@ -157,7 +140,7 @@ class SEResNextProjectionBlock(nn.Module):
         self.projection_conv = nn.Sequential(nn.LazyConv2d(out_channels=out_channels, 
                                                 kernel_size=1,
                                                 stride=stride, 
-                                                padding="same", 
+                                                #padding="same", 
                                                 bias=False
                                                 ),
                                 nn.LazyBatchNorm2d(),
@@ -174,7 +157,8 @@ class SEResNextProjectionBlock(nn.Module):
         
         self.group_conv = nn.Sequential(nn.LazyConv2d(out_channels=bottleneck_channels, 
                                                  kernel_size=3, stride=stride, 
-                                                 padding="same", bias=False, 
+                                                 padding=1, 
+                                                 bias=False, 
                                                  groups=cardinality
                                                 ),
                                         nn.LazyBatchNorm2d(),
@@ -336,13 +320,15 @@ def group(*,
           out_channels, 
           num_blocks,
           cardinality=32, reduction_ratio=16,
+          stride=2,
           variant: Literal["standard", "pre", "post", "identity", "enclose"] = "standard",
           **kwargs
           ):
     block_collection = []
     block = SEResNextProjectionBlock(bottleneck_width=bottleneck_width, out_channels=out_channels,
                                     cardinality=cardinality, reduction_ratio=reduction_ratio,
-                                    variant=variant
+                                    variant=variant,
+                                    stride=stride
                                     )
     block_collection.append(block)
     for _ in range(num_blocks -1):
@@ -357,8 +343,10 @@ def group(*,
 
 def learner(configs):
     group_blocks = []
-    for block_config in configs.block_config:
-        grp_blk = group(**configs._asdict(), **block_config._asdict())
+    for idx, block_config in enumerate(configs.block_config):
+        grp_blk = group(**configs._asdict(), **block_config._asdict(),
+                        stride=1 if idx == 0 else 2
+                        )
         group_blocks.append(grp_blk)
     return nn.Sequential(*group_blocks)
 
@@ -379,8 +367,9 @@ if __name__ == "__main__":
                 ResNextBlocksConfig(out_channels=2048, num_blocks=3)
                 ]
 
-    group_config = SEResNextGroupsConfig(cardinality=32, bottleneck_width=4, 
-                                         reduction_ratio=16,
+    group_config = SEResNextGroupsConfig(cardinality=32, 
+                                         bottleneck_width=4, 
+                                         reduction_ratio=32,
                                         variant="standard", 
                                         block_config=blocks
                                         )
