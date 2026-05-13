c1a4c874f12ea6eb0b39e609920e06392c27b05d
linus <lagbleze@st.ug.edu.gh>
2026-05-13 22:59:29 +0200

    feat. add MobileNetStemV2

--- src/mining_sites_detector/mobilenet_v2.py
+++ src/mining_sites_detector/mobilenet_v2.py
@@ -2,6 +2,22 @@ import torch
 import torch.nn as nn
 
 
+
+class MobileNetStemV2(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv = nn.LazyConv2d(out_channels=32, kernel_size=3,
+                                  padding=1,
+                                  bias=False,
+                                  stride=2
+                                  )
+        
+        
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+        
+
 class MobileNet_V2(nn.Module):
     def __init__(self, width_multiplier):
         pass
